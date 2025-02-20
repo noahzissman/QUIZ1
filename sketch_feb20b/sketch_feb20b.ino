2c509ada6c1b7c8952eb587d1a8be7bfdebb12ca
noahzissman <noahzissman@icloud.com>
2025-02-20 14:25:43 -0600

    Initial Creation

---
+++ sketch_feb20b/sketch_feb20b.ino
@@ -0,0 +1,15 @@
+void setup() {
+ const int redPin = 9;
+ const int greenPin = 10; 
+ const int bluePin = 11;
+
+ const int buttonRed = 2;
+ const int buttonGreen = 3;
+ const int buttonBlue = 4;
+
+}
+
+void loop() {
+  
+
+}
